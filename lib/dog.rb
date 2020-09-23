42543e56d06aeb5f555f93ed7aec756468b40b51
Crystal Norman <cnorm88@gmail.com>
2020-09-23 15:50:53 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+  
+  # Classical get method 
+  def name 
+    @name
+  end
+  
+  # Classical set method 
+  def name=(name) 
+    @name = name
+  end
+  
+  def breed=(breed) 
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end
+
+end
+ 
